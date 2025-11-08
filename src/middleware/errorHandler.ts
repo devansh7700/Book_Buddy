bbe280549d7aff2ea86e4fdd8a2141b9b3da97b1
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-08 00:18:07 -0600

    Added basic middleware setup

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  console.error(err);
+  res.status(400).json({ error: err.message || "Something went wrong" });
+};
